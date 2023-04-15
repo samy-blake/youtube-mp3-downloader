6ad66627e8192497cb5e416f72e3c92d6496f813
Soeren Balke <sb@fokuspokus-media.deb>
2023-04-15 16:13:17 +0200

    update progress data

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,11 @@
+import { videoInfo } from "ytdl-core";
+
 declare module YoutubeMp3Downloader {
   export interface IYoutubeMp3DownloaderOptions {
     ffmpegPath?: string;
     outputPath: string;
     // https://github.com/fent/node-ytdl-core/blob/0574df33f3382f3a825e4bef30f21e51cd78eafe/typings/index.d.ts#L7
-    youtubeVideoQuality?: 'lowest' | 'highest' | string | number;
+    youtubeVideoQuality?: "lowest" | "highest" | string | number;
     queueParallelism: number;
     progressTimeout: number;
     allowWebm?: boolean;
@@ -26,22 +28,33 @@ declare module YoutubeMp3Downloader {
       runtime: number;
       delta: number;
       speed: number;
-    }
+    };
+    fileName: string;
+    meta: videoInfo;
   }
 }
 
 declare class YoutubeMp3Downloader {
-  constructor(options: YoutubeMp3Downloader.IYoutubeMp3DownloaderOptions)
+  constructor(options: YoutubeMp3Downloader.IYoutubeMp3DownloaderOptions);
 
   cleanFileName(fileName: string): string;
   download(videoId: string, fileName?: string): void;
-  performDownload(task, callback: (errorNessage?: string, output?: any) => void): void;
+  performDownload(
+    task,
+    callback: (errorNessage?: string, output?: any) => void
+  ): void;
 
-  on(event: 'queueSize', listener: (total : number) => void): this;
-  on(event: 'error' | 'finished', listener: (err: any, data: any) => void): this;
-  on(event: 'progress', listener: (video: YoutubeMp3Downloader.IVideoTask) => void): this;
+  on(event: "queueSize", listener: (total: number) => void): this;
+  on(
+    event: "error" | "finished",
+    listener: (err: any, data: any) => void
+  ): this;
+  on(
+    event: "progress",
+    listener: (video: YoutubeMp3Downloader.IVideoTask) => void
+  ): this;
 }
 
-declare module 'youtube-mp3-downloader' {
+declare module "youtube-mp3-downloader" {
   export = YoutubeMp3Downloader;
 }
