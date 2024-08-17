27eaeaf8f518cf8e2ac0268ff6299e56ca15a7d0
samyblake <balkesoeren@googlemail.com>
2024-08-17 22:41:19 +0200

    fix ytdl-core

--- example/example.js
+++ example/example.js
@@ -2,13 +2,13 @@ var YoutubeMp3Downloader = require("../index");
 
 //Configure YoutubeMp3Downloader with your settings
 var YD = new YoutubeMp3Downloader({
-  ffmpegPath: "/usr/local/bin/ffmpeg", // FFmpeg binary location
+  ffmpegPath: "/opt/homebrew/bin/ffmpeg", // FFmpeg binary location
   queueParallelism: 2, // Download parallelism (default: 1)
   progressTimeout: 2000, // Interval in ms for the progress reports (default: 1000)
 });
 
 //Download video and save as MP3 file
-YD.download("Vhd6Kc4TZls");
+YD.download("RySKv1p-h8Q");
 
 YD.on("finished", function (err, data) {
   console.log(JSON.stringify(data));
